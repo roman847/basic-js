d9e295927225297f68498ab6a5b15b6eb472e343
Roman <romabogdan147@gmail.com>
2022-05-01 16:24:54 +0300

    feat: add new tasks (date)

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,11 +1,11 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Implement class DepthCalculator with method calculateDepth
  * that calculates deoth of nested array
- * 
+ *
  * @example
- * 
+ *
  * const depthCalc = new DepthCalculator();
  * depthCalc.calculateDepth([1, 2, 3, 4, 5]) => 1
  * depthCalc.calculateDepth([1, 2, 3, [4, 5]]) => 2
@@ -13,12 +13,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr) {
+    let levels = 1;
+    let count;
+    arr.forEach((el) => {
+      if (Array.isArray(el)) {
+        count = this.calculateDepth(el) + 1;
+        if (count > levels) levels = count;
+      }
+    });
+    return levels;
   }
 }
 
 module.exports = {
-  DepthCalculator
+  DepthCalculator,
 };
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,23 +1,48 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create transformed array based on the control sequences that original
  * array contains
- * 
+ *
  * @param {Array} arr initial array
  * @returns {Array} transformed array
- * 
+ *
  * @example
- * 
+ *
  * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
- * 
+ *
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(array) {
+  if (Array.isArray(array) == false) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+  let transformArray = [];
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === "--discard-prev") {
+      if (i > 0 && array[i - 2] !== "--discard-next") {
+        transformArray.pop();
+      }
+    } else if (array[i] === "--discard-next") {
+      if (i !== array.length - 1) {
+        i++;
+      }
+    } else if (array[i] === "--double-prev") {
+      if (i > 0 && array[i - 2] !== "--discard-next") {
+        transformArray.push(array[i - 1]);
+      }
+    } else if (array[i] === "--double-next") {
+      if (i !== array.length - 1) {
+        transformArray.push(array[i + 1]);
+      }
+    } else {
+      transformArray.push(array[i]);
+    }
+  }
+  return transformArray;
 }
 
 module.exports = {
-  transform
+  transform,
 };
